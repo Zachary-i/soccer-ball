b19d7c6d707bafae6036bad84daa126e5c8ff4fe
Zachary Inglis <zacharycinglis@gmail.com>
2023-11-15 17:54:49 -0600

    rendering a static circle of quads

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -17,7 +18,7 @@ public class Main extends Application{
 	public void start(Stage primaryStage) throws Exception {
 		GridPane root = new GridPane();
 		primaryStage.setTitle("Soccer Ball");
-		primaryStage.setScene(new Scene(root, 400, 300));
+		primaryStage.setScene(new Scene(root, 800, 600));
 		primaryStage.show();
 		
 		Polygon polygon = new Polygon();
@@ -28,25 +29,45 @@ public class Main extends Application{
 		    50.0, 0.0});
 		polygon.setFill(new Color(.5,1,.5,1));
 		polygon.setStroke(Color.BLACK);
-		root.add(polygon, 5, 5);
+		//root.add(polygon, 5, 5);
 		polygon.setTranslateX(150);
 		polygon.setTranslateY(150);
+		Pane group = makeSquareBallGroup(100,7);
 		
+		root.add(group, 0, 0);
+		group.setTranslateX(300);
+		group.setTranslateY(300);
 		
 	}
 	
-	public Pane makeSquareBallPane(float sideLength, int gridSize) {
-		Pane ballPane = new Pane();
+	public Pane makeSquareBallGroup(float sideLengthScaler, int gridSize) {
+		Pane ball = new Pane();
+		final float number =1.092f;
 		float[] origin = new float[] {gridSize/2f,gridSize/2f};
-		float[][][] points = new float[gridSize][gridSize][2];
+		float[][][] points = new float[gridSize+1][gridSize+1][2];
 		for(int i=0; i<=gridSize;i++) {
 			for(int j=0; j<=gridSize;j++) {
-				float dist=(j-origin[0])*(j-origin[0])+(i-origin[1]*(i-origin[1]));
-				points[i][j][0]=5;
+				float dist=(j-origin[0])*(j-origin[0])+(i-origin[1])*(i-origin[1]);
+				dist = (float) Math.sqrt(dist);
+				points[i][j][0]=(float) (origin[0]+((j-origin[0])*sideLengthScaler*(1/Math.pow(number, Math.pow(dist, 1.4)))));
+				points[i][j][1]=(float) (origin[1]+((i-origin[1])*sideLengthScaler*(1/Math.pow(number, Math.pow(dist, 1.4)))));
+				System.out.print(i+" "+j+" | "+points[i][j][0]+ ", "+points[i][j][1]+" "+dist+ "\n");
+			}
+		}
+		for(int i=0; i<gridSize;i++) {
+			for(int j=0; j<gridSize;j++) {
+				Polygon quad = new Polygon();
+				quad.getPoints().addAll(new Double[]{
+						(double) points[i][j][0], (double) points[i][j][1],
+						(double) points[i][j+1][0], (double) points[i][j+1][1],
+						(double) points[i+1][j+1][0], (double) points[i+1][j+1][1],
+						(double) points[i+1][j][0], (double) points[i+1][j][1],});
+				quad.setFill(new Color(Math.random(),Math.random(),Math.random(),1));
+				ball.getChildren().add(quad);
 			}
 		}
 		
-		return ballPane;
+		return ball;
 	}
 
 }
